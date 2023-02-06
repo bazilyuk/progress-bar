1b2de16d524adfc672960229bc60490ebc675355
Max Bazilyuk <maksimbazilyuk@gmail.com>
2023-02-06 00:16:26 -0500

    added breakpoints. added toggle button to allow the user to toggle between a progress bar with breakpoints and one without

--- src/progress-bar-exercise/solution/index.js
+++ src/progress-bar-exercise/solution/index.js
@@ -8,11 +8,25 @@ const RequestStatus = {
   START: 1,
   FINISH: 2,
 }
-const TIME_OUT = 14;
+// when need to stop progress
+const PROGRESS_END = 90;
+// duration of request
+const TIME_OUT = 15;
 const Solution = () => {
+  const [withBreakPoints, setWithBreakPoints] = useState(false);
   const [status, setStatus] = useState(RequestStatus.OFF);
   const [percent, setPercent] = useState(0);
   const [showProgress, setShowProgress] = useState(true);
+
+  const breakpoints = [20, 50, 75];
+
+  console.log(`percent: ${percent}`);
+  const delayProgress = (percentage) => {
+    // check if our progress around breakpoints
+
+    const period = PROGRESS_END / TIME_OUT;
+    return !!breakpoints.find(item => percentage >= item - period && percentage <= item + period);
+  }
 
   useEffect(() => {
     // This useEffect to make interval for progress bar from 0 till 90% for 15 seconds
@@ -23,8 +37,16 @@ const Solution = () => {
     if (status === RequestStatus.START) {
       interval = setInterval(() => {
         counter += 1;
-        setPercent(90 / TIME_OUT * counter);
+        if (withBreakPoints) {
+          // if user need breakpoints then check if we need to not show the progress
+          if (!delayProgress(PROGRESS_END / TIME_OUT * counter)) {
+            setPercent(PROGRESS_END / TIME_OUT * counter);
+          }
+        } else {
+          setPercent(PROGRESS_END / TIME_OUT * counter);
+        }
         if (counter === TIME_OUT) {
+          // after 15 second put request on pause
           clearInterval(interval);
         }
       }, 1000);
@@ -32,7 +54,7 @@ const Solution = () => {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [status]);
+  }, [status, withBreakPoints]);
 
   const onStart = () => {
     // start new request. Show progress bar and set it to 0%
@@ -53,7 +75,13 @@ const Solution = () => {
     }, 1000);
   }
 
+  const toggleBreakpoints = () => {
+    setWithBreakPoints((prevState) => !prevState);
+  }
+
   return <div>
+    <Button onClick={toggleBreakpoints}>{withBreakPoints ? 'Hide' : 'Show'} Breakpoints</Button>
+    <br />
     <ButtonWrapperStyled>
       <Button onClick={onStart} disabled={status === 1}>{status === RequestStatus.START ? 'loading...' : 'start request'}</Button>
       {status === RequestStatus.START ? <Button onClick={onStop} color="red">Finish Request</Button> : null}
